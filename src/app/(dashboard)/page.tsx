db9b760a015d49b58cde210f588af15992e8e7ae
yunusmujadidi <yunusmujadidi@gmail.com>
2025-11-19 08:27:37 +0700

    feat: setup project and app routes

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      <div>
+        <Button>Test123</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
